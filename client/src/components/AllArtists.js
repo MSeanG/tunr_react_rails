dc3760835807088b1e4d78ac4a8900b84568a058
Sean <sgilmore@michael-sean.com>
2017-09-05 14:48:25 -0400

    added global nav and style

---
+++ client/src/components/AllArtists.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AllArtist extends Component {
+  render(){
+    return (
+      <div>
+      hello from allartist
+      </div>
+    )
+  }
+  }
+  export default AllArtist;
---
+++ client/src/components/SignUp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+  render(){
+    return (
+      <div>
+      hello from SignUp
+      </div>
+    )
+  }
+  }
+  export default SignUp;
